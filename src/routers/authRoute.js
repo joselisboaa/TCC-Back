b97b7ac636d0ecc8e156d61cb544b84a12bd8b2d
joselisboaa <joselisboaa05@gmail.com>
2025-02-19 13:15:31 -0300

    feat: remove build files

--- src/routers/authRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-const express_1 = require("express");
-const authController_1 = require("../controllers/authController");
-exports.authRouter = new express_1.Router();
-exports.authRouter.get("/login", authController_1.AuthController.generateAuthUrl);
-exports.authRouter.get("/login/callback", authController_1.AuthController.callback);
-exports.authRouter.get("/login/verify", authController_1.AuthController.verifyIfUserWasLoggedIn);
